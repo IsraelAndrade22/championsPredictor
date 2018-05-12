1153a6a3184f76173ab02be3a354f2f0901abd5e
Noe Lomeli <nolomeli@csumb.edu>
2018-05-12 14:48:44 -0700

    add team attributes table

--- predictScript.R
+++ predictScript.R
@@ -7,6 +7,7 @@ Country <- dbGetQuery(db,"Select * from Country")
 Match <- dbGetQuery(db,"Select * from Match")
 League <- dbGetQuery(db,"Select * from League")
 Team <- dbGetQuery(db,"Select * from Team")
+Team_Attributes <- dbGetQuery(db,"Select * from Team_Attributes")
 
 head(Match)
 
