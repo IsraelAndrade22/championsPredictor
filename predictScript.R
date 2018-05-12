fc135bf68f38ee0791881161798d59a23d3c0532
Noe Lomeli <nolomeli@csumb.edu>
2018-05-11 21:21:25 -0700

    replace NAs with 0s

--- predictScript.R
+++ predictScript.R
@@ -8,14 +8,20 @@ Match <- dbGetQuery(db,"Select * from Match")
 League <- dbGetQuery(db,"Select * from League")
 Team <- dbGetQuery(db,"Select * from Team")
 
-Team = Team[,colMeans(is.na(Team)) == 0] 
-Match = Match[,colMeans(is.na(Match)) == 0] 
+head(Match)
+
+# replace all NAs with 0s
+Match = replace(Match, is.na(Match), 0)
+Team = replace(Team, is.na(Team), 0)
 
 summary(Match)
 summary(Team)
+str(Match)
 
 #Count the number of matches the particular team played at home
 home_match = table(Match$home_team_api_id)
 #Count the number of matches the particular team played at home
 away_match = table(Match$away_team_api_id)
+
+
 
